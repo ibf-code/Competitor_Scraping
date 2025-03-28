4a9107750450ddc24670f7d1b659a317d0e4530e
diego9621 <82180537+diego9621@users.noreply.github.com>
2025-03-28 13:52:28 +0100

    Update Documentation data.py

--- modules/data.py
+++ modules/data.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 import pandas as pd
 
+#Reads Productgroups.csv to map their productgroup id's on our productgroup KEY
 product_group_mapping = pd.read_csv("mapping/productgroups.csv")
 mapping_dict = dict(zip(product_group_mapping['competitor_product_group'].astype(str), 
                        product_group_mapping['ibf_product_group']))
 
 def get_ibf_product_group(category_id):
     return mapping_dict.get(str(category_id))
-
+  
+#Reads Varieties.csv to map their Variety id's on our Variety _KEYS
 variety_mapping = pd.read_csv("mapping/varieties.csv")
 variety_mapping_dict = dict(zip(variety_mapping['competitor_variety'].astype(str),
                                 variety_mapping['ibf_variety']))
@@ -35,7 +37,7 @@ def process_inventory_data(products, date):
         extracted_data.append({
             "created_at": today,
             "eta_date": date,
-            "state": "Kentucky",
+            "state": "Kentucky", #Needed as they apply different pricing per region  
             "competitor": "Mayesh",
             "grower_name": product.get("farm_name", "Unknown") if product.get("farm_name") else None,
             "grower_country": product.get("country_name", None),
@@ -48,13 +50,10 @@ def process_inventory_data(products, date):
             "variety_key": ibf_variety,
             "stem_length": extract_stem_length(product.get("grade_name")),  
             "color_name": product.get("color_name", None),
-
             "competitor_product_url": f"https://www.mayesh.com/{product['seo_url']}",
             "competitor_product_image": f"https://www.mayesh.com{product['image']}",
-
             "available_units": product["qty"],
             "stems_per_unit": product.get("unit_count", None), 
-
             "stem_price": float(product['price_per_stem']) if product.get("price_per_stem") else None,
             "unit_price": float(product['price_per_unit']) if product.get("price_per_unit") else 0,
             "base_price": product.get("main_landed_cost", 0),
